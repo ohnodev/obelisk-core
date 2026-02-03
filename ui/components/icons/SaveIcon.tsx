fef60a570f281cc114a55a27cb8baf6e20d5014a
James Powell <jmpowell1337@gmail.com>
2026-02-04 01:59:51 +0700

    Update Save and Load icons with custom SVG designs
    - Save icon: Download/arrow down SVG
    - Load icon: Upload SVG with file icon
    - Icons now match their functionality (save = download, load = upload)

--- ui/components/icons/SaveIcon.tsx
+++ ui/components/icons/SaveIcon.tsx
@@ -4,16 +4,14 @@ export default function SaveIcon({ className }: { className?: string }) {
       className={className}
       width="16"
       height="16"
-      viewBox="0 0 16 16"
-      fill="none"
+      viewBox="0 0 122.88 110.9"
+      fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M13 11V13.5C13 13.7761 12.7761 14 12.5 14H3.5C3.22386 14 3 13.7761 3 13.5V11M5 12L8 15M8 15L11 12M8 15V6"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M13.09,35.65h30.58V23.2l34.49,0v12.45l31.47,0L61.39,82.58L13.09,35.65L13.09,35.65z M61.44,97.88l47.51-0.14 l4.54-21.51l9.38,0.31v34.36L0,110.9V76.55l9.39-0.31l4.54,21.51L61.44,97.88L61.44,97.88L61.44,97.88z M43.67,0h34.49v4.62H43.67 V0L43.67,0z M43.67,9.32h34.49v9.44H43.67V9.32L43.67,9.32z"
       />
     </svg>
   );
