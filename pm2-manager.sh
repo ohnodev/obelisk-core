597db677462fd2afd15604c8ff898984ea2447df
James Powell <jmpowell1337@gmail.com>
2026-02-08 00:24:20 +0700

    fix: disable log compression, keep last 5 plain-text rotated logs

--- pm2-manager.sh
+++ pm2-manager.sh
@@ -93,8 +93,8 @@ setup_logrotate() {
         pm2 install pm2-logrotate
     fi
     pm2 set pm2-logrotate:max_size 10M
-    pm2 set pm2-logrotate:retain 7
-    pm2 set pm2-logrotate:compress true
+    pm2 set pm2-logrotate:retain 5
+    pm2 set pm2-logrotate:compress false
     pm2 set pm2-logrotate:dateFormat YYYY-MM-DD_HH-mm-ss
     pm2 set pm2-logrotate:workerInterval 30
     pm2 set pm2-logrotate:rotateInterval "0 0 * * *"
